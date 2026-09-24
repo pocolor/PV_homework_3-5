a04b840f021b83a71e9ff5a15f584f2de6d73f7e
pocolor <luvecjr@gmail.com>
2026-09-24 22:18:32 +0200

    add: statement 5 (unfinished)

---
+++ statement_05/main.py
@@ -0,0 +1,91 @@
+#!./.venv/bin/python3
+
+from pprint import pprint
+from collections.abc import MutableSequence, Iterable
+
+
+class DictList(MutableSequence):
+    def __init__(self, iterable: Iterable = None):
+        self._dict = dict()
+
+        if iterable is None:
+            return
+
+        if not isinstance(iterable, Iterable):
+            raise TypeException
+
+        self._dict = dict(enumerate(iterable))
+
+    def __getitem__(self, index):
+        if isinstance(index, slice):
+            raise NotImplemented
+
+        return self._dict[self._sanit_index(index)]
+
+    def __setitem__(self, index, value):
+        if isinstance(index, slice):
+            raise NotImplemented
+
+        self._dict[self._sanit_index(index)] = value
+
+    def __len__(self):
+        return len(self._dict)
+
+    def __iter__(self):
+        return iter(self._dict.values())
+
+    def _sanit_index(self, index):
+        if index < 0:
+            index += len(self)
+
+        if index < 0 or index >= len(self):
+            raise IndexError
+
+        return index
+
+    def append(self, element):
+        self._dict[len(self)] = element
+
+    def clear(self):
+        self._dict.clear()
+
+    def copy(self):
+        return DictList(iter(self))
+
+    def count(self, element):
+        counter = 0
+        for e in self._dict.values():
+            if e == element:
+                counter += 1
+        return counter
+
+    def extend(self, iterable):
+        self._dict = dict(enumerate((*self, *iterable)))
+
+    def index(self, element, start = 0, stop = len(self)):
+        for i in range(start, math.clamp(stop, 0, len(self))):
+            if self[i] == element:
+                return i
+
+        raise ValueError
+
+    def insert(self, index, element):
+        for i in range(self._sanit_index(index), len(self)):
+            ...
+    
+
+def main() -> None:
+
+    my_list = ["foo", "bar", "baz"]
+    my_dict = DictList(my_list)
+
+    
+    def print_both() -> None:
+        pprint(my_list)
+        pprint([my_dict[i] for i in range(len(my_dict))])
+
+
+if __name__ == "__main__":
+    print(dir(list))
+    main()
+
