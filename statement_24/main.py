cfc85c5c582b67bc73e1585f827c5011c0099103
pocolor <luvecjr@gmail.com>
2026-09-26 20:05:32 +0200

    add: stmt_24

---
+++ statement_24/main.py
@@ -0,0 +1,14 @@
+#!./.venv/bin/python3
+
+
+def main() -> None:
+    my_list = ["foo", "bar", "baz"]
+    my_set = set(my_list)
+
+    print(my_list[0])
+    print(my_set[0])
+
+
+if __name__ == "__main__":
+    main()
+
