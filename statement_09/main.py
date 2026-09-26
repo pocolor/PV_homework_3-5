953098476827cca98bd43c5a5ba609c52f594541
pocolor <luvecjr@gmail.com>
2026-09-26 16:44:29 +0200

    add: stmt_09

---
+++ statement_09/main.py
@@ -0,0 +1,12 @@
+#!./.venv/bin/python3
+
+
+def main() -> None:
+    my_dict = {"foo": "noot1", "bar": "noot2", "baz": "noot3"}
+
+    #print(my_dict[0])
+    print(my_dict.values()[0])
+
+
+if __name__ == "__main__":
+    main()
